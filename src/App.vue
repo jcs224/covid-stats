e396cf173e794186a69f0ab45369731d0cab9645
Joe Sweeney <joe.sweeney224@gmail.com>
2020-09-19 22:49:46 -0600

    add ability to change states

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,134 @@
 <template>
   <div class="flex flex-col h-screen">
     <h1 class="bg-green-500 text-white text-center text-4xl">COVID-19 Stats</h1>
+    <select v-model="stateSelected">
+      <option v-for="st in states" :key="st" :value="st[1]">{{ st[0] }}</option>
+    </select>
     <div class="p-3 bg-gray-200 flex-1">
-      <canvas class="bg-white" id="chart1"></canvas>
+      <canvas class="bg-white" id="positive-test-ratio"></canvas>
     </div>
   </div>
 </template>
 
 <script>
 import 'chart.js'
+import axios from 'axios'
+import { DateTime } from 'luxon'
 
 export default {
+  data() {
+    return {
+      positiveTestRatioChart: null,
+      stateSelected: 'MT',
+      states: [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+      ]
+    }
+  },
+  
+  methods: {
+    getStateData(state) {
+      var self = this
+
+      axios.get(`https://api.covidtracking.com/v1/states/${state.toLowerCase()}/daily.json`)
+      .then((result) => {
+        let ratios = result.data.slice(0, 30).reverse().map(r => {
+          let newDate = DateTime.fromISO(r.date).toISODate()
+
+          return {
+            x: newDate,
+            y: r.positiveIncrease / r.totalTestResultsIncrease
+          }
+        })
+        console.log(ratios)
+
+        for (let r of ratios) {
+          self.positiveTestRatioChart.data.labels.pop()
+          self.positiveTestRatioChart.data.datasets.forEach((dataset) => {
+            dataset.data.pop()
+          })
+        }
+
+        for (let r of ratios) {
+          self.positiveTestRatioChart.data.labels.push(r.x)
+          self.positiveTestRatioChart.data.datasets.forEach((dataset) => {
+            dataset.data.push(r)
+          })
+        }
+
+        self.positiveTestRatioChart.update()
+      })
+    }
+  },
+
   mounted() {
-    console.log(document.getElementById('chart1'))
-    let chartOne = new Chart(document.getElementById('chart1'), {
-      type: 'bar',
+    this.positiveTestRatioChart = new Chart(document.getElementById('positive-test-ratio'), {
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Ratio',
+            data: []
         }]
-    },
+      },
     })
+
+    this.getStateData(this.stateSelected)
+  },
+
+  watch: {
+    stateSelected(newState) {
+      this.getStateData(newState)
+    }
   }
 }
 </script>
